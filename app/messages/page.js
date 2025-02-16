c49d04d104410781af8d8de7f1b323555ed5ac21
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-16 14:49:37 +0000

    revalidate-dynamic-static

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,15 +1,21 @@
 import Messages from '@/components/messages';
+import { unstable_noStore } from 'next/cache';
+
+// export const revalidate = 5;
+// export const dynamic = 'force-static';
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    next: {
-      revalidate: 5
-    }
-    // cache: 'no-store'
-    // headers: {
-    //   'X-ID': 'page',
-    // },
-  });
+  unstable_noStore();
+  const response = await fetch('http://localhost:8080/messages');
+  // const response = await fetch('http://localhost:8080/messages', {
+  //   next: {
+  //     revalidate: 5
+  //   }
+  //   // cache: 'no-store'
+  //   // headers: {
+  //   //   'X-ID': 'page',
+  //   // },
+  // });
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
